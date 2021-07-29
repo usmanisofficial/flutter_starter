608c83231fb0fa7fc053b8d63dcde58885b38947
Muhammad Usman <usmanisofficial@outlook.com>
2021-07-29 22:25:09 +0500

    Day10 completed

---
+++ lib/pages/models/catalog.dart
@@ -0,0 +1,10 @@
+class Item {
+  final String item;
+  final String id;
+  final String desc;
+  final int price;
+  final String imageURL;
+  final String color;
+
+  Item(this.item, this.id, this.desc, this.price, this.imageURL, this.color);
+}
