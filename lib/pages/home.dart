4c98d968a1a518f217346b4c9c958184d839ec99
Muhammad Usman <usmanisofficial@outlook.com>
2021-07-30 21:52:38 +0500

    day13 completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_starter/models/catalog.dart';
 import 'package:flutter_starter/widgets/ItemWidget.dart';
 import 'package:flutter_starter/widgets/drawer.dart';
+import 'dart:convert';
 
 //git readded
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var itemsData = decodedData["products"]; 
+  }
 
   @override
   Widget build(BuildContext context) {
